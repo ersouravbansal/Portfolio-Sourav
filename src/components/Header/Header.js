d28ba7d506fcb89a4a47030379556fbb1c26d196
ersouravbansal <souravbansal277@gmail.com>
2023-04-09 14:11:07 +0530

    updated resume

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -82,7 +82,7 @@ function Header() {
 
           <Button
             onClick={() => {
-              window.open("https://drive.google.com/file/d/1I2s_aOeykZGcsfljlt695ZwHe_9rcKiR/view?usp=sharing");
+              window.open("https://drive.google.com/file/d/1rwKfUA_YUoIn-xUJcNdFmTuNc5sfdH-u/view?usp=sharing");
             }}
             className='resumebtn'><span>Resume</span>
           </Button>
