9baac8b47c16fd94549d8d60b018dff3da91a12a
ersouravbansal <souravbansal277@gmail.com>
2023-05-30 13:01:24 +0530

    updated footer.

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import '../../pages/style.css';
-import { Container } from 'react-bootstrap'
+import React from "react";
+import "../../pages/style.css";
+import { Container } from "react-bootstrap";
+import Footer from "../Footer";
 
 function Contactpage() {
   return (
-    <div className='contactbackground'>
-      <Container>
-        <h2 className='contacthead'>Get In Touch</h2>
-        <p className='contactpara'> I’m currently searching for opportunities for a React developer role. <br /> If there is any vacancy my inbox is always open. Whether <br /> you have any further questions or just want to say hi, <br /> I’ll try my best to get back to you!</p>
-        <button
-          className='contactbtn'
-          onClick={() => {
-            window.open("https://wa.me/+918950355518");
-          }}
-        >Say Hi!</button>
-        <span></span>
-        <hr className='line' />
-        <p className='copyright'>© Copyright 2023.
-        <br /> 
-        Designed & Built by <span>Sourav</span></p>
-      </Container>
-    </div>
-  )
+    <>
+      <div className="contactbackground">
+        <Container>
+          <h2 className="contacthead">Get In Touch</h2>
+          <p className="contactpara">
+            {" "}
+            I’m currently searching for opportunities for a React developer
+            role. <br /> If there is any vacancy my inbox is always open.
+            Whether <br /> you have any further questions or just want to say
+            hi, <br /> I’ll try my best to get back to you!
+          </p>
+          <button
+            className="contactbtn"
+            onClick={() => {
+              window.open("https://wa.me/+918950355518");
+            }}
+          >
+            Say Hi!
+          </button>
+          <Footer />
+        </Container>
+      </div>
+    </>
+  );
 }
 
-export default Contactpage
+export default Contactpage;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <span></span>
+      <hr className="line" />
+      <p className="copyright">
+        © Copyright 2023.
+        <br />
+        Designed & Built by <span>Sourav</span>
+      </p>
+    </>
+  );
+}
+
+export default Footer;
